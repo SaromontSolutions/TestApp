b71f681b8c9d11e238d78bd9a938438efa9db5a6
Tegan McNamara <teganmcnamara7@gmail.com>
2025-08-06 17:47:57 +0930

    eslint config update

---
+++ TestAppJs/eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig({
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "no-console": "warn",
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "no-unused-vars": "warn",
+  },
+});
