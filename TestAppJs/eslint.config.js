9b5f516d26d6e6367d9a4de7987fdb33777b3980
Tegan McNamara <teganmcnamara7@gmail.com>
2025-08-06 18:12:43 +0930

    eslint config update

--- TestAppJs/eslint.config.js
+++ TestAppJs/eslint.config.js
@@ -1,31 +1,33 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig({
-  languageOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-    globals: {
-      ...globals.browser,
-      ...globals.node,
-      ...globals.jest,
-    },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+/** @type {import("eslint").FlatConfig[]} */
+export default [
+  js.configs.recommended, 
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      "no-console": "warn",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "no-unused-vars": "warn",
+    },
   },
-  linterOptions: {
-    reportUnusedDisableDirectives: true,
-  },
-  rules: {
-    "no-console": "warn",
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"],
-    "no-unused-vars": "warn",
-  },
-  plugins: [],
-  configs: [js.configs.recommended],
-});
+];
+
